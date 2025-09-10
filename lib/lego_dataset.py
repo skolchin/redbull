a422dcdc92a400d9d02b522e717fefc5593fa066
kol <skolchin@gmail.com>
2025-09-10 12:42:32 +0300

    image / image+mask datasets split

--- lib/lego_dataset.py
+++ lib/lego_dataset.py
@@ -1,7 +1,9 @@
 # source: https://www.kaggle.com/datasets/martinellis/synthetic-lego-images/
 
 import cv2
+import json
 import torch
+import logging
 import numpy as np
 import pandas as pd
 import albumentations as A
@@ -9,35 +11,40 @@ from pathlib import Path
 from numpy.typing import NDArray
 from torch.utils.data import Dataset
 from albumentations.pytorch.transforms import ToTensorV2
-from typing import Dict, Literal, Tuple, Annotated, TypedDict, Union, cast
+from typing import Dict, Literal, Tuple, Annotated, TypedDict, Callable, cast
+
+_logger = logging.getLogger(__name__)
 
 FloatImageTensor = Annotated[ torch.Tensor, torch.float32, Literal[3, 'H', 'W'] ]
 IntImageTensor = Annotated[ torch.Tensor, torch.int64, Literal[3, 'H', 'W'] ]
-IntMaskTensor = Annotated[ torch.Tensor, torch.int64, Literal['H', 'W'] ]
+IntMaskTensor = Annotated[ torch.Tensor, torch.int64, Literal['N', 'H', 'W'] ]
 BoxesTensor = Annotated[ torch.Tensor, torch.int64, Literal['N', 4] ]
 AreaTensor = Annotated[ torch.Tensor, torch.float32, Literal['N'] ]
 IsCrowdTensor = Annotated[ torch.Tensor, torch.int64, Literal['N'] ]
 LabelsArray = Annotated[ NDArray[np.int_], Literal['N'] ]
-ColorArray = Annotated[ NDArray[np.int_], Literal[4] ]
-ColorListTensor = Annotated[ torch.Tensor, torch.int64, Literal['N', 3] ]
+ColorArray = Annotated[ NDArray[np.uint8], Literal[3] ]
+
+ImagePreprocessCallable = Callable[..., FloatImageTensor]
+""" Will be called with image=... kwarg """
+
+ImageWithMasksPreprocessCallable = Callable[..., Tuple[FloatImageTensor, IntMaskTensor]]
+""" Will be called with image=... mask=... kwargs """
 
 class TargetDict(TypedDict):
+    """ Target description for images """
     image_id: int
     boxes: BoxesTensor
     labels: LabelsArray
-    mask: FloatImageTensor | None
     area: AreaTensor
     iscrowd: IsCrowdTensor
+    image_fn: str
+    mask_fn: str | None
 
-class SynthentcLegoImagesDataset(Dataset):
-    """ Dataset object for Synthetic Lego images.
-
-    Supports indexing, both sequental and by image name (e.g. [`images/rgb_81.png`])
-    """
+class SynthentcLegoImagesDatasetBase(Dataset):
+    """ Base class """
 
     CLASSES_FILE: str = 'classes.csv'
     ANNOTATION_FILE: str = 'annotation-data.csv'
-    SEMANTIC_SEGMENTATION_DIR: str = 'semantic-segmentation'
 
     _splits: Dict[str, NDArray[np.int_]] | None = None
     _annotations: pd.DataFrame | None
@@ -66,10 +73,7 @@ class SynthentcLegoImagesDataset(Dataset):
 
     def __init__(self,
                  data_root: Path | str,
-                 split: Literal['train', 'valid'] | None = None,
-                 transforms: A.Compose | None = None,
-                 with_masks: bool = False,
-                 cache_size: int = 5):
+                 split: Literal['train', 'valid'] | None = None):
 
         super().__init__()
 
@@ -82,24 +86,50 @@ class SynthentcLegoImagesDataset(Dataset):
         self.image_ids = self._splits[split] if split else self.annotations['img_id'].unique()
 
         classes_df = pd.read_csv(self.data_root / self.CLASSES_FILE)
-        self.classes = pd.Series(classes_df.name.astype(str),index=classes_df.label_id.astype(int)).to_dict()
+        self.classes = pd.Series(classes_df['label_name'].astype(str), index=classes_df['label_id'].astype(int)).to_dict()
+        self.class_names = pd.Series(classes_df['name'].astype(str), index=classes_df['label_id'].astype(int)).to_dict()
 
-        self.transforms = transforms
-        self.with_masks = with_masks
-        self.cache_size = 5
+        self.transforms: A.Compose | None = None
+        match split:
+            case 'train':
+                self.transforms = self.get_transform_train()
+            case 'valid':
+                self.transforms = self.get_transform_valid()
 
-    def get(self, index: int | str) -> Tuple[FloatImageTensor, TargetDict, Union['SegmentationMask',None]] | None:
-        """ Get an entry. 
-        
-        Returns image tensor, targed definition and mask tensor (if `with_masks` was set to `True) or None` 
-        """
-        try:
-            return self[index]
-        except (KeyError, FileNotFoundError):
-            return None
+    def __len__(self) -> int:
+        """ Length """
+        return self.image_ids.shape[0]
 
-    def __getitem__(self, index: int | str) -> Tuple[FloatImageTensor, TargetDict, 'SegmentationMask']:
-        """ Get implementatio"""
+    @classmethod
+    def get_transform_train(cls) -> A.Compose:
+        return A.Compose([
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.2),
+            A.ToFloat(255.0, p=1.0),
+            ToTensorV2(transpose_mask=True, p=1.0)
+        ], bbox_params={'format':'pascal_voc', 'label_fields': ['labels']})
+
+    @classmethod
+    def get_transform_valid(cls) -> A.Compose:
+        return A.Compose([
+            A.ToFloat(255.0, p=1.0),
+            ToTensorV2(transpose_mask=True, p=1.0),
+        ], bbox_params={'format': 'pascal_voc', 'label_fields':['labels']})
+
+
+class SynthentcLegoImagesDataset(SynthentcLegoImagesDatasetBase):
+    """ Dataset object for Synthetic Lego images """
+
+    def __init__(self,
+                 data_root: Path | str,
+                 split: Literal['train', 'valid'] | None = None,
+                 preprocess_fn: ImagePreprocessCallable | None = None):
+
+        super().__init__(data_root, split)
+        self.preprocess_fn = preprocess_fn
+
+    def _get(self, index: int | str) -> Tuple[FloatImageTensor, TargetDict]:
+        """ Get implementation """
         image_id = self.image_ids[index] if isinstance(index, int) else index
         records = self.annotations[self.annotations['img_id'] == image_id]
         if records.empty:
@@ -112,15 +142,6 @@ class SynthentcLegoImagesDataset(Dataset):
         
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        mask_image = None
-        if self.with_masks:
-            mask_fn = image_fn.parent.parent / self.SEMANTIC_SEGMENTATION_DIR / image_id.split('/')[-1].replace('rgb', 'segmentation')
-            mask_image = cv2.imread(str(mask_fn), cv2.IMREAD_COLOR)
-            if mask_image is None:
-                raise FileNotFoundError(mask_fn)
-
-            mask_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2RGB)
-
         # mine are different, heithg & width not xmax & ymax
         boxes = records[['x', 'y', 'w', 'h']].values
         boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
@@ -128,103 +149,183 @@ class SynthentcLegoImagesDataset(Dataset):
 
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
         area = torch.as_tensor(area, dtype=torch.float32)
+        boxes = torch.as_tensor(boxes, dtype=torch.int64)
 
-        labels = records['labels'].values
-        # labels = torch.as_tensor(label, dtype=torch.int64)
+        labels = cast(LabelsArray, records['labels'].values)
 
         # suppose all instances are not crowd
         iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)
 
-        target: TargetDict = TargetDict(
+        target = TargetDict(
             image_id=int(records.index.min()),
-            boxes=cast(BoxesTensor, torch.as_tensor(boxes)),
-            labels=cast(LabelsArray, labels),
-            mask=None,
-            area=cast(AreaTensor, area),
-            iscrowd=cast(IsCrowdTensor, iscrowd),
+            boxes=boxes,
+            labels=labels,
+            area=area,
+            iscrowd=iscrowd,
+            image_fn=str(image_fn),
+            mask_fn=None,
         )
 
         if not self.transforms:
             # simply cast from uint to float
             image = torch.as_tensor(image).to(torch.float32) / 255.0
-            if mask_image is not None:
-                mask_image = torch.as_tensor(mask_image)
         else:
             sample = {
                 'image': image,
-                'mask': mask_image,
                 'bboxes': target['boxes'],
                 'labels': labels,
             }
             sample = self.transforms(**sample)
             image = sample['image']
-            mask_image = sample['mask']
 
-            target['boxes'] = cast(BoxesTensor, torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1,0))
+            target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1,0)
 
-        return cast(FloatImageTensor, image), target, SegmentationMask(mask_image)
+        if self.preprocess_fn:
+            image = self.preprocess_fn(image=image)
 
-    def __len__(self) -> int:
-        return self.image_ids.shape[0]
+        return image, target
+
+    def get(self, index: int | str) -> Tuple[FloatImageTensor, TargetDict] | None:
+        """ Get an entry """
+        try:
+            return self._get(index)
+        except (KeyError, FileNotFoundError):
+            return None
+
+    def __getitem__(self, index: int | str) -> Tuple[FloatImageTensor, TargetDict]:
+        """ Indexing """
+        return self._get(index)
 
     def __iter__(self):
+        """ Iteration """
         for i in range(len(self)):
             yield self[i]
 
-class SegmentationMask:
-    def __init__(self, mask: IntImageTensor | None):
-        if mask is None:
-            self._orig_mask = None
-            self._mask = None
-            self._objects = torch.as_tensor([], dtype=torch.int64)
-        else:
-            assert len(mask.shape) == 3
-            self._orig_mask = mask
-            self._mask = mask.permute(1, 2, 0) if mask.shape[0] == 3 else mask
-            self._objects = torch.unique(self._mask.reshape(-1, 3), dim=0)[1:]
+class SynthentcLegoImagesWithMasksDataset(SynthentcLegoImagesDatasetBase):
+    """ Dataset object for Synthetic Lego images with segmentation masks """
 
-    @property
-    def mask(self) -> IntImageTensor | None:
-        return self._orig_mask
+    ANNOTATION_DEFS_FILE: str = 'annotation-jsons/annotation_definitions.json'
+    SEMANTIC_SEGMENTATION_DIR: str = 'semantic-segmentation'
 
-    @property
-    def colors(self) -> ColorListTensor:
-        return self._objects
-
-    def __len__(self) -> int:
-        return len(self.colors)
-
-    def __getitem__(self, index: int) -> IntMaskTensor:
-        if self._mask is None:
-            raise KeyError(index)
+    def __init__(self,
+                 data_root: Path | str,
+                 split: Literal['train', 'valid'] | None = None,
+                 preprocess_fn: ImageWithMasksPreprocessCallable | None = None,
+                 remove_missing_labels: bool = False):
         
-        object_color = self._objects[index]
+        super().__init__(data_root, split)
+        self.preprocess_fn = preprocess_fn
+        self.remove_missing_labels = remove_missing_labels
 
-        object_mask = (self._mask == object_color)
-        object_mask = object_mask.all(dim=-1).reshape(self._mask.shape[:2])
+        ann_defs = json.loads((self.data_root / self.ANNOTATION_DEFS_FILE).read_text())
+        segm_defs = [x for x in ann_defs['annotation_definitions'] if x['name'] == 'semantic segmentation']
+        assert segm_defs, f'Cannot find semantic segmentation definition in {self.data_root / self.ANNOTATION_DEFS_FILE} file'
 
-        empty = torch.zeros(self._mask.shape[:2], dtype=torch.int64)
-        empty[object_mask] = 1
+        def to_clr(pixel_value: Dict) -> ColorArray:
+            return (np.array([pixel_value['r'], pixel_value['g'], pixel_value['b']], dtype=np.float32) * 255).round(0).astype(np.uint8)
 
-        return empty
+        label_to_id_mapping = {v: k for k, v in self.classes.items()}
+        self.class_colors = {label_to_id_mapping[spec['label_name']]: to_clr(spec['pixel_value']) for spec in segm_defs[0]['spec']}
+
+    def _get(self, index: int | str) -> Tuple[FloatImageTensor, IntMaskTensor, TargetDict]:
+        """ Get implementation """
+        image_id = self.image_ids[index] if isinstance(index, int) else index
+        records = self.annotations[self.annotations['img_id'] == image_id]
+        if records.empty:
+            raise KeyError(index)
+
+        image_fn = self.data_root / image_id
+        image = cv2.imread(str(image_fn), cv2.IMREAD_COLOR)
+        if image is None:
+            raise FileNotFoundError(image_fn)
+        
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        mask_fn = image_fn.parent.parent / self.SEMANTIC_SEGMENTATION_DIR / image_id.split('/')[-1].replace('rgb', 'segmentation')
+        mask_image = cv2.imread(str(mask_fn), cv2.IMREAD_COLOR)
+        if mask_image is None:
+            raise FileNotFoundError(mask_fn)
+
+        mask_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2RGB)
+
+        # mine are different, heigth & width not xmax & ymax
+        boxes = records[['x', 'y', 'w', 'h']].values
+        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
+        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
+        boxes = torch.as_tensor(boxes, dtype=torch.int64)
+
+        labels = cast(LabelsArray, records['labels'].values)
+
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((records.shape[0],), dtype=torch.int64)
+
+        # parse mask by colors
+        masks = []
+        for lbl, clr in self.class_colors.items():
+            empty = torch.zeros(mask_image.shape[:2], dtype=torch.int64)
+            if lbl in labels:
+                clr_mask = (mask_image == clr)
+                object_mask = clr_mask.all(axis=-1).reshape(mask_image.shape[:2])
+                if object_mask.any():
+                    empty[object_mask] = 1
+                elif self.remove_missing_labels:
+                    _logger.warning(f'Cannot find segmentation mask for label {lbl} on {mask_fn}, removing label from target')
+                    boxes = boxes[ labels != lbl ]
+                    labels = labels[ labels != lbl ]
+                else:
+                    _logger.warning(f'Cannot find segmentation mask for label {lbl} on {mask_fn}')
+            
+            masks.append(empty)
+
+        masks = np.stack(masks, axis=0)
+
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = torch.as_tensor(area, dtype=torch.float32)
+
+        target = TargetDict(
+            image_id=int(records.index.min()),
+            boxes=boxes,
+            labels=labels,
+            area=area,
+            iscrowd=iscrowd,
+            image_fn=str(image_fn),
+            mask_fn=str(mask_fn),
+        )
+
+        if not self.transforms:
+            # simply cast from uint to float
+            image = torch.as_tensor(image).to(torch.float32) / 255.0
+            masks = torch.as_tensor(masks)
+        else:
+            sample = {
+                'image': image,
+                'masks': masks,
+                'bboxes': target['boxes'],
+                'labels': labels,
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+            masks = sample['masks']
+
+            target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1,0)
+
+        if self.preprocess_fn:
+            image, masks = self.preprocess_fn(image=image, mask=masks)
+
+        return image, masks, target
+
+    def get(self, index: int | str) -> Tuple[FloatImageTensor, IntMaskTensor, TargetDict] | None:
+        """ Get an entry """
+        try:
+            return self._get(index)
+        except (KeyError, FileNotFoundError):
+            return None
+
+    def __getitem__(self, index: int | str) -> Tuple[FloatImageTensor, IntMaskTensor, TargetDict]:
+        """ Indexing """
+        return self._get(index)
 
     def __iter__(self):
+        """ Iteration """
         for i in range(len(self)):
             yield self[i]
-
-    def __array__(self, copy: bool = False) -> np.ndarray | None:
-        return self._orig_mask.detach().cpu().numpy() if self._orig_mask is not None else None
-
-def get_transform_train():
-    return A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.RandomBrightnessContrast(p=0.2),
-        A.ToFloat(255.0, p=1.0),
-        ToTensorV2(transpose_mask=True, p=1.0)
-    ], bbox_params={'format':'pascal_voc', 'label_fields': ['labels']})
-
-def get_transform_valid():
-    return A.Compose([
-        A.ToFloat(255.0, p=1.0),
-        ToTensorV2(transpose_mask=True, p=1.0),
-    ], bbox_params={'format': 'pascal_voc', 'label_fields':['labels']})
